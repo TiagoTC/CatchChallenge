3d6c1f86acc95d5e6754bb6cd3871ce1268f96ca
Tiago <tiago16tc@yahoo.com.br>
2017-08-19 19:09:34 +1200

    Added Network Unit Test

---
+++ app/src/test/java/acatch/tiago/catchchallenge/network/NetworkTests.kt
@@ -0,0 +1,27 @@
+package acatch.tiago.catchchallenge.network
+
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+
+/**
+ * Created by tiago on 19/08/17.
+ */
+class NetworkTests{
+
+	private val network: INetwork
+
+	init {
+		network = Network()
+	}
+
+	@Test
+	@Throws(Exception::class)
+	fun test_get_items_call() {
+
+		val callResponse = network.getItems()
+		val items = callResponse.blockingGet()
+
+		assertNotNull(items)
+		assert(items.isNotEmpty())
+	}
+}
